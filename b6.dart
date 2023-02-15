3941aa34bd9d98749a83c63af8c38df1b4ca10b9
quangdo2412 <100840252+quangdo2412@users.noreply.github.com>
2023-02-15 17:18:19 +0700

    Add files via upload

---
+++ b6.dart
@@ -0,0 +1,7 @@
+void main(){
+    for (int i=5 ; i<=5; i++){
+      for (int j=1; j<=10; j++){
+        print('$i * $j = ${i*j}' );
+      }
+    }
+}
---
+++ b8.dart
@@ -0,0 +1,8 @@
+void main(){
+  int a = 5;
+  int b = 10;
+  print('$a + $b = ${a+b}');
+  print('$a - $b = ${a-b}');
+  print('$a * $b = ${a*b}');
+  print('$a / $b = ${a/b}');
+}
